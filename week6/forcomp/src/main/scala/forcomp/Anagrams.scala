d0adc581efe740d1515ea05efb87f5a052455c73
unCovered <alexgalvez8292@outlook.com>
2016-12-21 16:44:57 +0100

    One more method finished

--- week6/forcomp/src/main/scala/forcomp/Anagrams.scala
+++ week6/forcomp/src/main/scala/forcomp/Anagrams.scala
@@ -57,9 +57,7 @@ object Anagrams {
   lazy val dictionaryByOccurrences: Map[Occurrences, List[Word]] = dictionary.groupBy(w => wordOccurrences(w))
 
   /** Returns all the anagrams of a given word. */
-  def wordAnagrams(word: Word): List[Word] = ???
-//    dictionaryByOccurrences.get(wordOccurrences(word))
-
+  def wordAnagrams(word: Word): List[Word] = dictionaryByOccurrences(wordOccurrences(word))
 
 
   /** Returns the list of all subsets of the occurrence list.
