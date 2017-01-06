fe0d9083ec6caa1a310f6425ce17b701ba91af9b
Alejandro Gálvez <alexgalvez8292@outlook.com>
2017-01-06 20:37:10 +0100

    Added sqrt examples

---
+++ week1/sqrt_v3.scala
@@ -0,0 +1,17 @@
+object sqrt {
+  def sqrt(x: Double) = {
+    def abs(x: Double) = if (x < 0) -x else x
+
+    def sqrtIter(guess: Double): Double =
+      if (isGoodEnough(guess)) guess
+      else sqrtIter(improve(guess), x)
+
+    def isGoodEnough(guess: Double) = 
+      abs(guess * guess - x) / x < 0.001
+
+   def improve(guess: Double) =
+      (guess + x / guess) / 2
+ 
+    sqrtIter(1.0)
+  }
+}
